f230bf0aa5033b078ee0d6e0329a26d5e810d6dd
Brenno Nunes <brennons.06@gmail.com>
2026-05-22 20:23:03 -0500

    Aula 1 de JavaScript

---
+++ 22-05-2026/script.js
@@ -0,0 +1,75 @@
+//Console serve para declarar mensagens, como erros, confirmações e entre outros tipos
+console.log("BOA NOITE! HOJE É SEXTA FEIRA")
+console.error("Este é um erro!")
+console.warn("Este é um aviso")
+
+//Variáveis
+
+var x = 10
+
+//Variáveis modernas
+
+let y = 15 //Permite que o valor seja alteravel
+
+const z = 20 
+
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(x+y+z)
+
+//Texto
+
+const name = "Brenno Nunes da Silva Barros"
+console.log(name)
+console.log(typeof name)
+
+//Numero Decimal
+
+const decimal = 20.1
+
+console.log(decimal)
+
+console.log(typeof decimal)
+
+//Booleano (True or False)
+
+const isApproved = false
+
+console.log(isApproved)
+
+console.log(typeof isApproved)
+
+//Null
+
+let surname = null
+
+console.log(surname)
+
+console.log(typeof surname)
+
+//Array
+
+let languages = ["JavaScript0, PHP, Python"]
+
+console.log(languages)
+
+console.log(typeof languages)
+
+//Object
+
+let user = {email: "brennons.06@gmail.com", password: "12345", age: "30"}
+
+console.log(user)
+
+console.log(typeof user)
+
+//Metodo de string
+
+const fullname = "Brenno Nunes da Silva Barros"
+
+console.log(fullname)
+
+console.log(fullname.length)
+
+console.log(typeof fullname)
